90a97cb4dfd20042821f01e25444aae4323d95b9
20162026 <36726858+20162026@users.noreply.github.com>
2020-09-27 21:41:41 +0300

    solved

--- IS_lab_1_template.m
+++ IS_lab_1_template.m
@@ -1,5 +1,6 @@
 %Classification using perceptron
-
+clc
+clear
 %Reading apple images
 A1=imread('apple_04.jpg');
 A2=imread('apple_05.jpg');
@@ -31,7 +32,8 @@ metric_A3=apvalumas_roundness(A3); %roundness
 %4th apple image(A4)
 hsv_value_A4=spalva_color(A4); %color
 metric_A4=apvalumas_roundness(A4); %roundness
-%5th apple image(A5)hsv_value_A5=spalva_color(A5); %color
+%5th apple image(A5)
+hsv_value_A5=spalva_color(A5); %color
 metric_A5=apvalumas_roundness(A5); %roundness
 %6th apple image(A6)
 hsv_value_A6=spalva_color(A6); %color
@@ -78,54 +80,115 @@ w1 = randn(1);
 w2 = randn(1);
 b = randn(1);
 
-% calculate weighted sum with randomly generated parameters
-%v1 = <...>; % write your code here
-% calculate current output of the perceptron 
-if v > 0
-	y = 1;
-else
-	y = -1;
-end
-% calculate the error
-e1 = T(1) - y;
 
-% repeat the same for the rest 4 inputs x1 and x2
-% calculate wieghted sum with randomly generated parameters
-% v2 = <...> ; % write your code here
-% calculate current output of the perceptron 
-if v > 0
-	y = 1;
-else
-	y = -1;
-end
-% calculate the error
-e2 = T(2) - y;
-
-% <...> write the code for another 3 inputs
-
-% calculate the total error for these 5 inputs 
-e = abs(e1) + abs(e2) + abs(e3) + abs(e4) + abs(e5);
-
-% write training algorithm
-while e ~= 0 % executes while the total error is not 0
-	% here should be your code of parameter update
-%   calculate output for current example
-% 
-%   calculate error for current example
-% 
-%   update parameters using current inputs ant current error
-% 	w1 = 
-%   w2 = 
-%   b = 
-% 
-%   Test how good are updated parameters (weights) on all examples used for training
-%   calculate outputs and errors for all 5 examples using current values of the parameter set {w1, w2, b}
-%   calculate 'v1', 'v2', 'v3',... 'v5'
-% 
-%   calculate 'y1', ..., 'y5'
-%     
-%   calculate 'e1', ... 'e5'
+n=0.02;
+e=[];
+%for kekw=[1:1000]
+    for i= [1 2 3 4 5]
     
-	% calculate the total error for these 5 inputs 
-	e = abs(e1) + abs(e2) + abs(e3) + abs(e4) + abs(e5);
+        % calculate weighted sum with randomly generated parameters
+        %v1 = <...>; % write your code here
+        v=x1(i)*w1+x2(i)*w2+b;
+        % calculate current output of the perceptron 
+        if v > 0
+        	y = 1;
+        else
+        	y = -1;
+        end
+        % calculate the error
+        e(i) = T(i) - y;
+        
+        w1=w1+n*e(i)*x1(i);
+        w2=w2+n*e(i)*x2(i);
+        b=b+e(i)*n;
+    end
+    e_abs = abs(e(1)) + abs(e(2)) + abs(e(3)) + abs(e(4)) + abs(e(5));
+%end
+iter=0;
+a=0;
+% write training algorithm
+while e_abs ~=0
+    while (e_abs ~=0  & a<1000000)  % executes while the total error is not 0
+        
+        for i= [1 2 3 4 5]
+        
+            % calculate weighted sum with randomly generated parameters
+            %v1 = <...>; % write your code here
+            v=x1(i)*w1+x2(i)*w2+b;
+            % calculate current output of the perceptron 
+            if v > 0
+            	y = 1;
+            else
+            	y = -1;
+            end
+            % calculate the error
+            e(i) = T(i) - y;
+            
+            w1=w1+n*e(i)*x1(i);
+            w2=w2+n*e(i)*x2(i);
+            b=b+e(i)*n;
+        end
+        e_abs = abs(e(1)) + abs(e(2)) + abs(e(3)) + abs(e(4)) + abs(e(5));
+    	% calculate the total error for these 5 inputs '
+       
+        iter=iter+1;
+        a=a+1;
+    	
+    end
+    a=0;
+disp('iterations');
+disp(iter);
 end
+
+% disp(iter)
+% disp(w1)
+% disp(w2)
+% disp(b)
+% disp(e)
+% disp(e_abs)
+
+
+%tikriname atsakyma
+%hsv_value_A1  A9  P1 P4
+%metric_A1 A9     P1  P4
+disp('----testing----')
+
+
+x1_test=[hsv_value_A1 hsv_value_A2 hsv_value_A3 hsv_value_A4 hsv_value_A5 hsv_value_A6 hsv_value_A7 hsv_value_A8 hsv_value_A9 hsv_value_P1 hsv_value_P2 hsv_value_P3 hsv_value_P4 ];
+x2_test=[metric_A1 metric_A2 metric_A3 metric_A4 metric_A5 metric_A6 metric_A7 metric_A8 metric_A9 metric_P1 metric_P2 metric_P3 metric_P4 ];
+P_test=[x1_test;x2_test]';
+close all;
+
+plot(x1_test(1:9),x2_test(1:9),'xg')
+hold on
+plot(x1_test(10:13),x2_test(10:13),'xr')
+
+
+
+syms xx yy
+f(xx,yy) = xx*w1+yy*w2+b;
+figure(1)
+fc = fcontour(f,[0 1 0 1]);
+fc.LevelList = [0 0];
+
+ 
+
+
+v_test=w1*x1_test+w2*x2_test+b;
+for i=1:length(v_test)
+    if v_test(i) > 0
+    	y_test(i) = 1;
+    else
+    	y_test(i) = -1;
+    end
+end
+disp(y_test)
+
+disp('naive Bayes')
+P_inv=P';
+ 
+ 
+Mdl = fitcnb(P_inv,T);
+label = (predict(Mdl ,P_test ))';
+
+disp(label)
